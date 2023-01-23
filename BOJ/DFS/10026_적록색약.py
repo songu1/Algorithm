8d73ac9d695f86e2ddf724a89bc17aa0e7a1aea6
songu1 <75112062+songu1@users.noreply.github.com>
2023-01-23 16:45:41 +0900

    python 수정

---
+++ BOJ/DFS/10026_적록색약.py
@@ -0,0 +1,58 @@
+# 크기 n*n인 그리디에 RGB 중 하나 색칠
+# 구역은 같은색 - 몇개의 구역
+# 같은 색상이 상하좌우로 인접 -> 같은 구역
+# 적록색약이아닌 사람 인접한 R,G,B로 구역의 수
+# 적록색약인 사람 인접한 RG,B로 구역의 수
+# 적록색약인 사람이 봤을 때 ,아닌 사람이 봣을 때 구역의 수
+
+# 입력 : n
+# n개의 줄에 그림이 주어짐
+
+# 출력 : 적록색약이 아닌 경우 구역의 수 / 적록색약인 사람 구역의 수
+
+import sys
+
+# 입력
+n=int(sys.stdin.readline())
+picX=[]
+picO=[]
+for i in range(n):
+    picX.append(list(map(str,sys.stdin.readline().rstrip())))
+    # picO.append(picX[i])
+    picO.append(picX[i].replace('G','R'))
+print(picO)
+
+resX=0
+resO=0
+
+# dfs 함수
+def dfs(pic,x,y,color):
+    if x<0 or y<0 or x>=n or y>=n:
+        return False
+    if pic[x][y]=='O':
+        return False
+    elif pic[x][y]==color:
+        pic[x][y]='O'
+        dfs(pic,x-1,y,color)
+        dfs(pic,x+1,y,color)
+        dfs(pic,x,y-1,color)
+        dfs(pic,x,y+1,color)
+        return True
+    return False
+
+for i in range(n):
+    for j in range(n):
+        if dfs(picX,i,j,picX[i][j])==True:
+            resX+=1
+        if dfs(picO,i,j,picO[i][j])==True:
+            resO+=1
+print(picX)
+print(picO)
+print(resX,resO)
+
+# 5
+# RRRBB
+# GGBBB
+# BBBRR
+# BBRRR
+# RRRRR       # 4 3
