8f9a35be81546701f7a1085eeaf0a82db321ddfd
songu1 <75112062+songu1@users.noreply.github.com>
2023-01-23 21:12:44 +0900

    적록색약/골드5

--- BOJ/BFS/10026_적록색약.py
+++ BOJ/BFS/10026_적록색약.py
@@ -10,6 +10,54 @@
 
 # 출력 : 적록색약이 아닌 경우 구역의 수 / 적록색약인 사람 구역의 수
 
+import sys
+from collections import deque
+
+# bfs 함수
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+def bfs(graph,x,y,color):
+    queue=deque()
+    if graph[x][y]=='O':
+        return False
+    queue.append((x,y))
+    graph[x][y]='O'
+    while queue:
+        x,y=queue.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx<0 or ny<0 or nx>=n or ny>=n:
+                continue
+            if graph[nx][ny]==color:
+                queue.append((nx,ny))
+                graph[nx][ny]='O'
+    return True
+
+# 입력
+n=int(sys.stdin.readline())
+normal=[]
+colorX=[[] for _ in range(n)]
+for i in range(n):
+    normal.append(list(map(str,sys.stdin.readline().rstrip())))
+    for j in range(n):
+        if normal[i][j]=='G':
+            colorX[i].append('R')
+        else:
+            colorX[i].append(normal[i][j])
+
+nor=0
+col=0
+for i in range(n):
+    for j in range(n):
+        if bfs(normal,i,j,normal[i][j])==True:
+            nor+=1
+        if bfs(colorX,i,j,colorX[i][j])==True:
+            col+=1
+print(nor, col)
+        
+
+
 
 
 
