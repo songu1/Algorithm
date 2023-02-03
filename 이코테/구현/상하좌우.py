197198ea263bcd2ca892f84a4bb65e9c7b7f6b34
songu1 <75112062+songu1@users.noreply.github.com>
2023-02-03 21:56:53 +0900

    이코테 연습문제-상하좌우

--- 이코테/구현/상하좌우.py
+++ 이코테/구현/상하좌우.py
@@ -7,12 +7,32 @@
 # 첫째줄 : 공간의 크기 n (1<=N<=100)
 # 둘째줄 : a가 이동할 계획서 (1<=이동횟수<=100)
 
+# 요구사항대로 충실하면 되는 문제
+# 시뮬레이션 유형
+
+
+n=int(input())
+paths=list(map(str,input().split()))
+x,y=1,1
+dx=[0,0,-1,1]
+dy=[-1,1,0,0]
+move_types=['L','R','U','D']
+
+for path in paths:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if path==move_types[i]:
+            nx=x+dx[i]
+            ny=y+dy[i]
+        
+    if nx<1 or ny<1 or nx>n or ny>n:
+            continue
+    # 이동 수행
+    x,y=nx,ny
+
+print(x,y)
+
+
+
 # 5
 # R R R U D D           #3 4
-
-N=int(input())
-path=list(map(str,input().split()))
-
-
-
-print(path)
