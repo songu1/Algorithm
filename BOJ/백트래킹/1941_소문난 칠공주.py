964911b1b671d4f04fe742bade22e8342e7d4c1a
songu1 <75112062+songu1@users.noreply.github.com>
2023-06-29 18:13:53 +0900

    소문난 칠공주/골드3

--- BOJ/백트래킹/1941_소문난 칠공주.py
+++ BOJ/백트래킹/1941_소문난 칠공주.py
@@ -19,6 +19,63 @@
     # 2. 뽑은 조합이 연달아 있는지 확인() - BFS
     # 3. 백트래킹
 
+# 풀이 1 - 공간복잡도 - 90000정도
+
+# import sys
+# seat=[]
+# for i in range(5):
+#     seat = seat + list(map(str,sys.stdin.readline().rstrip()))
+# visited=[False]*25
+# resList=[]
+# case = 0
+# count = 0
+
+# # 조합으로 7개의 수 뽑기
+# idxList = []
+# def combinations(idx,arr,arrIdx):
+#     if arr.count("Y") >= 4:
+#         return
+#     if len(arr) == 7:
+#         idxList.append(arrIdx)
+#         # if arr.count("S")>=4:
+#         #     idxList.append(arrIdx)
+#         return
+#     for i in range(idx,25):
+#         combinations(i+1, arr + [seat[i]], arrIdx + [i])
+
+# combinations(0,[],[])
+
+# # dfs 함수
+# dx=[-5,5,-1,1]
+# def dfs(arr,x,visited):
+#     global count
+#     # 방문처리
+#     visited[x] = True
+#     count += 1
+#     if count == 7:
+#         return True
+#     # 주변 탐색
+#     for i in range(4):
+#         nx = x + dx[i]
+#         # print(nx)
+#         if nx <0 or nx>=25 or (i==2 and nx%5 == 4) or (i==3 and nx%5==0): # 상하좌우 값 X
+#             continue
+#         if not visited[nx] and nx in arr:
+#             if dfs(arr,nx,visited):
+#                 return True
+#     return False
+    
+# # main 함수
+# for ilist in idxList:
+#     if dfs(ilist,ilist[0],visited[:]):
+#         resList.append(ilist)
+#         case += 1
+#     count = 0
+
+# print(case)
+
+
+# 풀이 2 - 공간복잡도 줄어듦 (30000정도)
 
 import sys
 seat=[]
@@ -28,21 +85,6 @@ visited=[False]*25
 resList=[]
 case = 0
 count = 0
-
-# 조합으로 7개의 수 뽑기
-idxList = []
-def combinations(idx,arr,arrIdx):
-    if arr.count("Y") >= 4:
-        return
-    if len(arr) == 7:
-        idxList.append(arrIdx)
-        # if arr.count("S")>=4:
-        #     idxList.append(arrIdx)
-        return
-    for i in range(idx,25):
-        combinations(i+1, arr + [seat[i]], arrIdx + [i])
-
-combinations(0,[],[])
 
 # dfs 함수
 dx=[-5,5,-1,1]
@@ -63,25 +105,23 @@ def dfs(arr,x,visited):
             if dfs(arr,nx,visited):
                 return True
     return False
-    
-# main 함수
-for ilist in idxList:
-    if dfs(ilist,ilist[0],visited[:]):
-        resList.append(ilist)
-        case += 1
-    count = 0
+
+# 조합으로 7개의 수 뽑기
+def combinations(idx,arr,arrIdx):
+    global case, count
+    if arr.count("Y") >= 4:
+        return
+    if len(arr) == 7:
+        if dfs(arrIdx,arrIdx[0],visited[:]):
+            case += 1
+        count = 0
+        return
+    for i in range(idx,25):
+        combinations(i+1, arr + [seat[i]], arrIdx + [i])
+
+combinations(0,[],[])
 
 print(case)
-
-
-# for i in range(5):
-#     for j in range(5):
-#         visited[i][j] = True
-#         backtracking([seat[i][j]],i,j,[(i,j)])
-#         visited = [[False]*5 for _ in range(5)]
-
-# print(case)
-# print(len(resList))
 
 
 
@@ -96,30 +136,6 @@ print(case)
 # YYSYY
 # YYYYY
 # YYSYY       # 1
-
-# YYYYY
-# SYSYS
-# YYYYY
-# YSYYS
-# YYYYS 
-
-# YYYYY
-# YYYYS
-# YYYYS
-# YYYYS
-# YYYYS
-
-# YYYYY
-# SYSYS
-# YSYYY
-# YYYYS
-# YYYYY 
-
-# SSSSS
-# SSSSS
-# SSSSS
-# SSSSS
-# SSSSY
 
 # 힌트
 # .....    .....
