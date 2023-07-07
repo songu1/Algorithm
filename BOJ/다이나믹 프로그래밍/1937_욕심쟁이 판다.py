389c52f52d5766af69536186df1d653473569bd8
songu1 <75112062+songu1@users.noreply.github.com>
2023-07-07 19:45:58 +0900

    욕심쟁이 판다/골드3

--- BOJ/다이나믹 프로그래밍/1937_욕심쟁이 판다.py
+++ BOJ/다이나믹 프로그래밍/1937_욕심쟁이 판다.py
@@ -8,15 +8,40 @@
 # 대나무 숲의 정보 (각 지역의 대나무 양 : 정수값, 1~1000000)
 # 출력 : 판다가 이동할 수 있는 칸의 수의 최댓값
 
+# DP + DFS
+
 import sys
+sys.setrecursionlimit(1000000)
 
 n = int(sys.stdin.readline())
 forest=[]
 for _ in range(n):
     forest.append(list(map(int,sys.stdin.readline().split())))
+dp = [[0]*n for _ in range(n)]
 
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+def dfs(forest,x,y,dp):
+    # 방문처리
+    dp[x][y] = 1
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if nx<0 or nx>=n or ny<0 or ny>=n:
+            continue
+        if forest[nx][ny] > forest[x][y]:
+            if dp[nx][ny] == 0:
+                dfs(forest,nx,ny,dp)
+            dp[x][y] = max(dp[x][y], dp[nx][ny]+1)
+    return
 
-
+maxValue = 0
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == 0:
+            dfs(forest,i,j,dp)
+        maxValue = max(maxValue,dp[i][j])
+print(maxValue)
 
 # 4
 # 14 9 12 10
