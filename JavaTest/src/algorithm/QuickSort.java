c8c6d8a8f701da34d51fab66a7a9a14d7a0bec09
Yujeong Song <syj0002012@naver.com>
2025-12-26 09:27:52 +0900

    Update QuickSort.java

--- JavaTest/src/algorithm/QuickSort.java
+++ JavaTest/src/algorithm/QuickSort.java
@@ -1,4 +1,31 @@
 package algorithm;
 
 public class QuickSort {
+  public static void quickSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int pivot = arr[(left + right) / 2];
+        int l = left;
+        int r = right;
+
+        while (l <= r) {
+            while (arr[l] < pivot) l++;
+            while (arr[r] > pivot) r--;
+
+            if (l <= r) {
+                swap(arr, l, r);
+                l++;
+                r--;
+            }
+        }
+
+        quickSort(arr, left, r);
+        quickSort(arr, l, right);
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 }
